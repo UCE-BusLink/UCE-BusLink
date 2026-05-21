56777188a2696ffa0c43f905259a22661833efb1
ldalomoto <ldalomoto@uce.edu.ec>
2026-05-20 20:11:13 -0500

    feat: SCRUM-39 Backend Core Multi-Module Architecture Initialization

---
+++ uce-buslink-backend/uce-buslink-main/src/main/java/com/ucebuslink/main/UceBuslinkApplication.java
@@ -0,0 +1,11 @@
+package com.ucebuslink.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.ucebuslink")
+public class UceBuslinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UceBuslinkApplication.class, args);
+    }
+}
