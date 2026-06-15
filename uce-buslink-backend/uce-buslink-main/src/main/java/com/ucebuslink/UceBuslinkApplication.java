ecbbe413a467ec814adf9be02416b1d808d1414b
ldalomoto <ldalomoto@uce.edu.ec>
2026-06-15 04:25:05 -0500

    perf(fleet): HU-391 implement redis caching strategy and slf4j on route service

--- uce-buslink-backend/uce-buslink-main/src/main/java/com/ucebuslink/UceBuslinkApplication.java
+++ uce-buslink-backend/uce-buslink-main/src/main/java/com/ucebuslink/UceBuslinkApplication.java
@@ -2,8 +2,10 @@ package com.ucebuslink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = "com.ucebuslink")
+@EnableCaching
 public class UceBuslinkApplication {
     public static void main(String[] args) {
         SpringApplication.run(UceBuslinkApplication.class, args);
