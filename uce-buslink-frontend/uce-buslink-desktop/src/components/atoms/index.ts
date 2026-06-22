5516860982731c122424107bdb6d994176db4af8
Kennet <kennet_rl@hotmail.com>
2026-06-21 22:33:25 -0500

    feat(seat-selection): atomize SeatSelectionPage

--- uce-buslink-frontend/uce-buslink-desktop/src/components/atoms/index.ts
+++ uce-buslink-frontend/uce-buslink-desktop/src/components/atoms/index.ts
@@ -1,3 +1,5 @@
 export { Button } from './Button';
 export { Spinner, RouteCardSkeleton } from './Spinner';
 export { TrustScoreRing } from './TrustScoreRing';
+export { SeatButton } from './SeatButton';
+export { LegendItem } from './LegendItem';
